19151717c64d59c25d00d0608a6ed1e47cb15853
Felipe Teixeira Moraes <felipetm6@hotmail.com>
2018-10-02 14:44:06 -0300

    Criar primeiro commit

---
+++ script.js
@@ -0,0 +1,36 @@
+/* numeroInteiro: 1;
+numeroQuebrado: 0.5;
+
+textoAspas: "Renan";
+textoApostrofo: 'Renan';
+textoCrase: `Renan`;
+concatenacaoDeTexto: 'Renan' + ' Rodrigo';
+textoCaixaAlta: 'Renan'.toUpperCase();
+textoCaixaBaixa: 'Renan'.toLowerCase();
+
+booleano: true;
+operacaoNao: !true;
+operacaoE: true && false;
+operacaoOu: true || false;
+notANumber: 45 / "Não sei";
+
+equalidade: 0 == 0;
+diferenca: 0 != 0;
+maior: 1 > 0;
+menor: 0 < 1;
+maiorQue: 1 >= 1;
+menorQue: 1 <= 1;
+
+equalidadeComTipo: 0 === 0;
+diferencaComTipo: 0 !== 0; */
+
+let nome = 'Renan Rodrigo';
+let idade = 28;
+let vivo = true;
+
+console.log('O Script foi executado corretamente.');
+console.log(nome);
+console.log(idade);
+console.log(vivo);
+
+alert(`Olá ${nome}!`);
